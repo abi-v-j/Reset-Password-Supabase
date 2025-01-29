0b7a908ff6607bded4c8c2e295b363002a0e09c3
abi-v-j <abijoy611@gmail.com>
2025-01-29 18:17:00 +0530

    change

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'YOUR_SUPABASE_URL'; // Replace with your Supabase URL
-const supabaseKey = 'YOUR_SUPABASE_ANON_KEY'; // Replace with your Supabase Anon Key
+const supabaseUrl = 'https://wtqzqtnofaxczcpohdfw.supabase.co'; // Replace with your Supabase URL
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind0cXpxdG5vZmF4Y3pjcG9oZGZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzExNTcwODcsImV4cCI6MjA0NjczMzA4N30.9q09jTP1QuyrDNmIxSoafAnFbaQ70rhQhBBda-c4zGY'; // Replace with your Supabase Anon Key
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
